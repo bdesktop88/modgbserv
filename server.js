351cce033a67f8d451e827b438560149db9cd495
bdesktop88 <b88942504@gmail.com>
2025-06-19 15:19:52 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const redirectsPath = path.join(__dirname, 'redirects.json');
 
 // Session middleware
