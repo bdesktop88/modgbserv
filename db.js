75ab3b9df10941898493bae4a6dbba5dfe6c7081
bdesktop88 <b88942504@gmail.com>
2025-06-20 08:25:41 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,51 +1,31 @@
-// db.js
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/redirects';
+mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-// Schema definition
 const redirectSchema = new mongoose.Schema({
     key: { type: String, required: true, unique: true },
     destination: { type: String, required: true },
     token: { type: String, required: true },
-});
+}, { timestamps: true });
 
-// Model
 const Redirect = mongoose.model('Redirect', redirectSchema);
 
-// Exported database operations
+// Add a new redirect
+function addRedirect(key, destination, token, callback) {
+    const newRedirect = new Redirect({ key, destination, token });
+    newRedirect.save(callback);
+}
+
+// Get a redirect by key
+function getRedirect(key, callback) {
+    Redirect.findOne({ key }, callback);
+}
+
 module.exports = {
-    // Add a new redirect
-    addRedirect: async (key, destination, token) => {
-        const entry = new Redirect({ key, destination, token });
-        await entry.save();
-    },
-
-    // Get a specific redirect
-    getRedirect: async (key) => {
-        return await Redirect.findOne({ key });
-    },
-
-    // Get all redirects (for admin view)
-    getAllRedirects: async () => {
-        return await Redirect.find({});
-    },
-
-    // Update a redirect's destination
-    updateRedirect: async (key, newDestination) => {
-        return await Redirect.findOneAndUpdate(
-            { key },
-            { destination: newDestination },
-            { new: true } // Return the updated document
-        );
-    },
-
-    // Delete a redirect
-    deleteRedirect: async (key) => {
-        return await Redirect.deleteOne({ key });
-    },
+    addRedirect,
+    getRedirect
 };
