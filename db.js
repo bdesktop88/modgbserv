351b6dcf0614216c34f68a6002ac0682fad46000
bdesktop88 <b88942504@gmail.com>
2025-06-20 09:26:28 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,64 +1,40 @@
 const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/redirects';
-mongoose.connect(mongoURI, {
-    // No need for these options anymore in mongoose 6+
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-});
+mongoose.connect(mongoURI);
 
 const redirectSchema = new mongoose.Schema({
-    key: { type: String, required: true, unique: true },
-    destination: { type: String, required: true },
-    token: { type: String, required: true },
+  key: { type: String, required: true, unique: true },
+  destination: { type: String, required: true },
+  token: { type: String, required: true },
 }, { timestamps: true });
 
 const Redirect = mongoose.model('Redirect', redirectSchema);
 
-// Add a new redirect
-async function addRedirect(key, destination, token, callback) {
-    try {
-        const newRedirect = new Redirect({ key, destination, token });
-        await newRedirect.save();
-        callback(null);
-    } catch (err) {
-        callback(err);
-    }
+// Add new redirect
+async function addRedirect(key, destination, token) {
+  const newRedirect = new Redirect({ key, destination, token });
+  return await newRedirect.save();
 }
 
-// Get a redirect by key
-async function getRedirect(key, callback) {
-    try {
-        const redirect = await Redirect.findOne({ key }).exec();
-        callback(null, redirect);
-    } catch (err) {
-        callback(err);
-    }
+// Get redirect by key
+async function getRedirect(key) {
+  return await Redirect.findOne({ key });
 }
 
-// Get all redirects
-async function getAllRedirects() {
-    return Redirect.find().exec();
+// Update destination
+async function updateRedirect(key, newDestination) {
+  return await Redirect.findOneAndUpdate({ key }, { destination: newDestination }, { new: true });
 }
 
-// Update a redirect's destination by key
-async function updateRedirect(key, destination) {
-    return Redirect.findOneAndUpdate(
-        { key },
-        { destination },
-        { new: true }
-    ).exec();
-}
-
-// Delete a redirect by key
+// Delete redirect
 async function deleteRedirect(key) {
-    return Redirect.findOneAndDelete({ key }).exec();
+  return await Redirect.findOneAndDelete({ key });
 }
 
 module.exports = {
-    addRedirect,
-    getRedirect,
-    getAllRedirects,
-    updateRedirect,
-    deleteRedirect,
+  addRedirect,
+  getRedirect,
+  updateRedirect,
+  deleteRedirect,
 };
