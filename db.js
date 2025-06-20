9b31bada5e506564b6493ea5fd3b43aa1c9cae6b
bdesktop88 <b88942504@gmail.com>
2025-06-20 08:52:03 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/redirects';
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose.connect(mongoURI);
 
 const redirectSchema = new mongoose.Schema({
     key: { type: String, required: true, unique: true },
@@ -14,13 +11,18 @@ const redirectSchema = new mongoose.Schema({
 
 const Redirect = mongoose.model('Redirect', redirectSchema);
 
-// Add a new redirect
-function addRedirect(key, destination, token, callback) {
-    const newRedirect = new Redirect({ key, destination, token });
-    newRedirect.save(callback);
+// Add a new redirect using Promises
+async function addRedirect(key, destination, token, callback) {
+    try {
+        const newRedirect = new Redirect({ key, destination, token });
+        await newRedirect.save();
+        callback(null); // success
+    } catch (err) {
+        callback(err); // pass error
+    }
 }
 
-// Get a redirect by key
+// Get a redirect by key (this is fine)
 function getRedirect(key, callback) {
     Redirect.findOne({ key }, callback);
 }
