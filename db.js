33fc1589f28f0f943afce1fd27b651eb1bc4e159
bdesktop88 <b88942504@gmail.com>
2025-06-20 09:12:39 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/redirects';
-mongoose.connect(mongoURI);
+mongoose.connect(mongoURI, {
+    // No need for these options anymore in mongoose 6+
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+});
 
 const redirectSchema = new mongoose.Schema({
     key: { type: String, required: true, unique: true },
@@ -12,17 +16,49 @@ const redirectSchema = new mongoose.Schema({
 const Redirect = mongoose.model('Redirect', redirectSchema);
 
 // Add a new redirect
-async function addRedirect(key, destination, token) {
-    const newRedirect = new Redirect({ key, destination, token });
-    await newRedirect.save();
+async function addRedirect(key, destination, token, callback) {
+    try {
+        const newRedirect = new Redirect({ key, destination, token });
+        await newRedirect.save();
+        callback(null);
+    } catch (err) {
+        callback(err);
+    }
 }
 
 // Get a redirect by key
-async function getRedirect(key) {
-    return await Redirect.findOne({ key });
+async function getRedirect(key, callback) {
+    try {
+        const redirect = await Redirect.findOne({ key }).exec();
+        callback(null, redirect);
+    } catch (err) {
+        callback(err);
+    }
+}
+
+// Get all redirects
+async function getAllRedirects() {
+    return Redirect.find().exec();
+}
+
+// Update a redirect's destination by key
+async function updateRedirect(key, destination) {
+    return Redirect.findOneAndUpdate(
+        { key },
+        { destination },
+        { new: true }
+    ).exec();
+}
+
+// Delete a redirect by key
+async function deleteRedirect(key) {
+    return Redirect.findOneAndDelete({ key }).exec();
 }
 
 module.exports = {
     addRedirect,
-    getRedirect
+    getRedirect,
+    getAllRedirects,
+    updateRedirect,
+    deleteRedirect,
 };
